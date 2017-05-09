506d45ff6e6d539e6619b2a06af64b27afce7a01
Saurabh K Sharma <saurabh.k.sharma@in.abb.com>
2017-05-09 14:09:25 +0530

    added deployment
    Signed-off-by: Saurabh K Sharma <saurabh.k.sharma@in.abb.com>

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -4,12 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject1
 {
     [TestClass]
+    //[DeploymentItem(@"UnitTestProject1\bin\Debug")]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
             Console.WriteLine("My first test case.");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("My second test case.");
         }
     }
 }
